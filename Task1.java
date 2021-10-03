b4d71beb8932446c72e3efde59c52a0f83166249
Samitha Perera <91825809+Ketha95@users.noreply.github.com>
2021-10-03 15:57:06 +0530

    Updated Task1 file

--- Task1.java
+++ Task1.java
@@ -1,5 +1,12 @@
 public class Task1{
   public static void main(String[] args) {
     System.out.println("Hello World");
+    System.out.println("Welcome to the GitHuB");
+    int x= 5;
+    int y=8;
+    int z = x+y;
+    System.out.println("z");
+
+
   }
 }
